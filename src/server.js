867f2e179fec9c70d52aa948ff354c89a3c97f7e
Victor Motta <victor.mottag@gmail.com>
2023-01-19 16:26:16 -0300

    Configurações iniciais feita!

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
 const PORT = 5000;
+const app = express();
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+try {
+    await mongoClient.connect();
+    db = mongoClient.db();
+    console.log("MongoDB Connected!");
+} catch (err) {
+    console.log(err.message);
+}
 
 app.use(cors());
 app.use(express.json());
